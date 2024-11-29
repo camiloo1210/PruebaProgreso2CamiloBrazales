a5182368c11bd6e808b57922e9469834b6816700
CamiloBrazales <laboratorio@UPOAULA10602>
2024-11-29 07:24:03 -0500

    Creacion page 1 y 2 dentro de carpeta Views

---
+++ PruebaProgreso2CamiloBrazales/Views/Page1.xaml.cs
@@ -0,0 +1,9 @@
+namespace PruebaProgreso2CamiloBrazales.Views;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PruebaProgreso2CamiloBrazales/Views/Page2.xaml.cs
@@ -0,0 +1,9 @@
+namespace PruebaProgreso2CamiloBrazales.Views;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+}
