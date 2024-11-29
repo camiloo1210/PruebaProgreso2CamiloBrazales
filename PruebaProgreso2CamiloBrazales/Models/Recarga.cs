300af567c40b7009588a22a68a51483068dff4b3
CamiloBrazales <laboratorio@UPOAULA10602>
2024-11-29 08:10:08 -0500

    Creacion Carpeta Models y creacion modelo Recarga

---
+++ PruebaProgreso2CamiloBrazales/Models/Recarga.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaProgreso2CamiloBrazales.Models
+{
+    class Recarga
+    {
+        public string numeroTelefono { get; set; }
+        public string nombreRecarga { get; set; }
+    }
+}
